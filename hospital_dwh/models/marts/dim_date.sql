857855b145cacdd6933b1c468c4a9861be748abe
nerson-olo <nerson.mampionona@olo.mg>
2026-05-05 15:11:11 +0300

    feat(mart): create dim_date with calendar attributes

---
+++ hospital_dwh/models/marts/dim_date.sql
@@ -0,0 +1,19 @@
+{{ config(materialized='table') }}
+
+select distinct
+    format_date('%Y%m%d', DateArrivee) as DateID,
+    DateArrivee as DateComplete,
+
+    extract(day from DateArrivee) as Jour,
+    extract(month from DateArrivee) as Mois,
+    format_date('%B', DateArrivee) as NomMois,
+    extract(quarter from DateArrivee) as Trimestre,
+    extract(year from DateArrivee) as Annee,
+    format_date('%A', DateArrivee) as JourSemaine,
+
+    case
+        when extract(dayofweek from DateArrivee) in (1, 7) then true
+        else false
+    end as WeekendFlag
+
+from {{ ref('stg_visits') }}
